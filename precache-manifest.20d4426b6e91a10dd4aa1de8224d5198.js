5c1c6df66c9e5f6c77f4d4a4ee4e6980b381daa8
Alen <alen.rajher@gmail.com>
2020-07-01 19:10:46 +0200

    Updates

---
+++ precache-manifest.20d4426b6e91a10dd4aa1de8224d5198.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff9b3994cf0eeda509e21c5444cf3932",
+    "url": "/weather_react_app/index.html"
+  },
+  {
+    "revision": "28e071d7c08561587a80",
+    "url": "/weather_react_app/static/css/main.ce98d77e.chunk.css"
+  },
+  {
+    "revision": "60f9874fe2d12508a5e0",
+    "url": "/weather_react_app/static/js/2.7e8df61b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather_react_app/static/js/2.7e8df61b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28e071d7c08561587a80",
+    "url": "/weather_react_app/static/js/main.b3df4adc.chunk.js"
+  },
+  {
+    "revision": "610e8e4f27b4a815ac68",
+    "url": "/weather_react_app/static/js/runtime-main.92d87db3.js"
+  }
+]);
